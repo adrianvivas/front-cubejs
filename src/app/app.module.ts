006fa1f6dea84b85b87d702429e2e746e8aa661e
Adrian Jacobo Vivas Bautista <adrianj.vivasb@gmail.com>
2020-07-03 10:33:28 -0500

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CubejsClientModule } from '@cubejs-client/ngx'; 
+import { ChartsModule } from 'ng2-charts';
+
+const cubejsOptions = {
+  token:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.K9PiJkjegbhnw4Ca5pPlkTmZihoOm42w8bja9Qs2qJg",
+  options: {
+    apiUrl: "https://react-query-builder.herokuapp.com/cubejs-api/v1"
+  }
+};
 
 @NgModule({
   declarations: [
@@ -10,7 +20,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule,
+    CubejsClientModule.forRoot(cubejsOptions)
   ],
   providers: [],
   bootstrap: [AppComponent]
